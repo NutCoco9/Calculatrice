95893fd132423937cafec92a0fd9784f7435de24
Ruben Kaplan <152229063+Diamagnetised@users.noreply.github.com>
2024-02-24 18:05:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ const info = document.querySelector('.infos')
 const allBtns = document.querySelectorAll('input[type=button]')
 
 function val(resultat){
-    if (!isNaN(parseFloat(resultat)) && isFinite(resultat)) {
-    resultat = resultat.replace(/\./g, ',');
-    }
     form.fenetre.value = form.fenetre.value + resultat
 }
 
