e64622e1cfd3129dd24ff8c999a722c9be3f7030
NutCoco9 <121462483+NutCoco9@users.noreply.github.com>
2024-02-25 09:22:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,40 +12,16 @@ function cos(resultat) {
     form.fenetre.value = Math.cos(resultat*Math.PI/180).toFixed(2)
 }
 
-
 function sin(resultat) {
     form.fenetre.value = Math.sin(resultat*Math.PI/180).toFixed(2)
 }
-
 
 function tan(resultat) {
     form.fenetre.value = Math.tan(resultat*Math.PI/180).toFixed(2)
 }
 
 
-function arcos(resultat) {
-    form.fenetre.value = Math.arccos(resultat*Math.PI/180).toFixed(2)
-}
 
-
-function arcsin(resultat) {
-    form.fenetre.value = Math.arcsin(resultat*Math.PI/180).toFixed(2)
-}
-
-
-function arctan(resultat) {
-    form.fenetre.value = Math.arctan(resultat*Math.PI/180).toFixed(2)
-}
-
-
-function sqrt(resultat) {
-    form.fenetre.value = Math.sqrt(resultat).toFixed(2)
-}
-
-
-function pi() {
-    return Math.PI
-}
 
 function calcule(){
     if(form.fenetre.value == ""){
@@ -55,8 +31,7 @@ function calcule(){
                 info.innerText = ""
             })
         }) 
-    }
-    else {
+    }else {
         const container = document.getElementById('fenetre').value;
 
         if (container.includes('cos(')) {
@@ -71,20 +46,33 @@ function calcule(){
             let result = text.replace(`cos(${x})`, `${vcos}`)
             form.fenetre.value = result
 
+        }  if (container.includes('sin(')){
+            var txt2= form.fenetre.value
+            init = txt2.indexOf('(');
+            fin = txt2.indexOf(')');
+            var x = txt2.substr(init+1,fin-init-1);
+
+            var vsin =  Math.sin(x*Math.PI/180).toFixed(2);
+            
+            let text2 = form.fenetre.value
+            let result = text2.replace(`sin(${x})`, `${vsin}`)
+            form.fenetre.value = result
+        }  if (container.includes('tan(')){
+            var txt3= form.fenetre.value
+            init = txt3.indexOf('(');
+            fin = txt3.indexOf(')');
+            var x = txt3.substr(init+1,fin-init-1);
+
+            var vtan =  Math.tan(x*Math.PI/180).toFixed(2);
+            
+            let text3 = form.fenetre.value
+            let result = text3.replace(`tan(${x})`, `${vtan}`)
+            form.fenetre.value = result
         }
-        if (container.includes(',')) {
-            form.fenetre.value = container.replace(/,/g, '.');
-        }
-        if (container.includes('x')) {
-            form.fenetre.value = form.fenetre.value.replace(/x/g, '*');        }
-        let a=eval(form.fenetre.value);
-        a=a.toString();
-        if(a.includes(".")){
-            a=a.replace(/\./g, ',');
-        }
-        form.fenetre.value = a;
-        info.innerText = "";
+        form.fenetre.value = eval(form.fenetre.value);
+        info.innerText = ""
     }
+    
 
 }
 
@@ -93,20 +81,5 @@ function suppr(){
 }
 
 function func(){
-    trigo.style = "visibility:hidden";
-    hist.style = "visibility:hidden";
     funcvar.style = "visibility:visible";
-}
-
-function trigof(){
-    funcvar.style = "visibility:hidden";
-    hist.style = "visibility:hidden";
-    trigo.style = "visibility:visible";
-}
-
-
-function histf(){
-    funcvar.style = "visibility:hidden";
-    trigo.style = "visibility:hidden";
-    hist.style = "visibility:visible";
 }
